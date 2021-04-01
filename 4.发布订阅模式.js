5522c0f7a5ba94cd03cdae17bbf88be4ca1a2885
ziWei <2515421994@qq.com>
2021-04-01 17:32:48 +0800

    发布订阅

--- 4.发布订阅模式.js
+++ 4.发布订阅模式.js
@@ -1,4 +1,5 @@
-//事件中心
+
+//解耦合
 const fs = require('fs');//发布订阅模式 核心就是把多个方法暂存起来，最后一次执行
 let events = {
   _events:[],
@@ -16,10 +17,24 @@ events.on(()=>{
 });
 
 let arr = [];
+events.on((data)=>{
+  arr.push(data);
+})
+
+events.on((data)=>{
+  if(arr.length==2){//最终结果还是计数器
+    console.log("读取完毕",arr);
+  }
+})
+
+let arr = [];
 fs.readFile('./a.txt','UTF8',function (err,data) {
-  out(data,0);
+  events.emit(data);
 })
 
 fs.readFile('./b.txt','UTF8',function (err,data) {
-  out(data,1);
+  events.emit(data);
 })
+
+//观察者模式 vue2 基于发布订阅的（发布订阅之间是没有依赖关系的）
+//对于我们的观察者模式：观察者 被观察者 vue3有发布订阅 没有观察者模式 没有使用class(不用继承)
---
+++ 5.观察者模式.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: ziwei
+ * @Date: 2021-04-01 17:32:21
+ * @LastEditors: ziwei
+ * @LastEditTime: 2021-04-01 17:32:38
+ */
+//观察者模式基于类
