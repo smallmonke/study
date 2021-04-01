a9fe630f8884890adee21b69003716dade48ab5b
ziWei <2515421994@qq.com>
2021-04-01 18:59:13 +0800

    基础promise实现 添加备注

--- source/1.promise.js
+++ source/1.promise.js
@@ -5,7 +5,7 @@
  * @Autor: ziwei
  * @Date: 2021-04-01 18:00:26
  * @LastEditors: ziwei
- * @LastEditTime: 2021-04-01 18:54:27
+ * @LastEditTime: 2021-04-01 18:58:48
  */
 let PENDING = "PENDING";
 let FULLFILLed = "FULLFIlled";
@@ -13,19 +13,19 @@ let REJECTED = "REJECTED";
 
 class Promise{
   constructor(executor){
-    this.status = PENDING
-    this.value = undefined;
-    this.reason = undefined;
-    const resolve = (value)=>{
+    this.status = PENDING;//promise默认的状态
+    this.value = undefined;//成功的原因
+    this.reason = undefined;//失败的原因
+    const resolve = (value)=>{//成功的resolve函数
       if(this.status === PENDING){
         this.value = value;
         this.status = FULLFILLed;
       }
     }
 
-    const reject = (reason)=>{
+    const reject = (reason)=>{//失败的函数
       if(this.status === PENDING){
-        this.status = REJECTED;
+        this.status = REJECTED;//修改状态
         this.reason = reason;
       }
     }
@@ -33,10 +33,11 @@ class Promise{
     try {
       executor(resolve,reject)
     } catch (e) {
+      //一旦抛出异常也执行失败
       reject(e);
     }
   }
-  then(onFulfilled,onRejected){
+  then(onFulfilled,onRejected){//onFulfilled onRejected promise+规定的2个函数
     if(this.status == FULLFILLed){
       onFulfilled(this.value);
     }
