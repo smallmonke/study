dc862a87f8e6822874e2936ae54ab9283f0e3463
ziWei <2515421994@qq.com>
2021-04-02 14:23:28 +0800

    增加笔记

--- 8.promise链式调用.js
+++ 8.promise链式调用.js
@@ -1,23 +1,47 @@
 /*
- * @Description: 
+ * @Description:
  * @Version: 1.0
  * @Autor: ziwei
  * @Date: 2021-04-02 09:39:04
  * @LastEditors: ziwei
- * @LastEditTime: 2021-04-02 10:28:00
+ * @LastEditTime: 2021-04-02 12:13:29
  */
 //promise的特点：解决了什么问题：1 链式调用解决嵌套回调的问题 2：同步并发问题
 //1.
-const fs = require('fs');//上一个人的输出是下一个的输入
+const fs = require('fs'); //上一个人的输出是下一个的输入
+let Promise = require('./source/3.promise');
 
-function readFile(filePath,encoding='UTF8') {
-  return new Promise((resolve,reject)=>{
-    fs.readFile(filePath,encoding,function (err,data) {
-      if(err){return reject(err)}
+function readFile(filePath, encoding = 'UTF8') {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, encoding, function (err, data) {
+      if (err) {
+        return reject(err);
+      }
       resolve(data);
-    })
-  })
+    });
+  });
 }
+
+let promise2 = new Promise((resolve) => {
+  resolve(1);
+}).then((data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('ok');
+    }, 1000);
+  });
+},(err)=>{
+  return 111
+});
+promise2.then(
+  (data) => {
+    console.log(data);
+  },
+  (err) => {
+    console.log('error', err);
+  }
+);
+//分析————————————————————————————————————————————
 
 //1.promise的链式调用 当调用then方法后会但会一个新的promise
 //情况1:then中方法返回的是一个（普通值 不是promise 包括undefined|没有设置值默认返回undefined）的情况，会作为外层下一次then的成功结果
@@ -26,16 +50,16 @@ function readFile(filePath,encoding='UTF8') {
 //无论上一次then走的是成功还是失败，只要返回的是普通值 都会执行下一次then的成功
 
 //总结：如果返回一个普通值（除了promise）就会传递给下一个then的成功，如果返回一个失败的promise或者抛出异常，会走下一个then的失败
-readFile('./a.txt','UTF8').then((value)=>{
-  return readFile(value);
-},(err)=>{
-  // 返回的是普通纸
-  return new Error();
-}).then((data)=>{
-  console.log('s',data);
-},()=>{
-  console.log("fail");
-})
+// readFile('./a.txt','UTF8').then((value)=>{
+//   return readFile(value);
+// },(err)=>{
+//   // 返回的是普通纸
+//   return new Error();
+// }).then((data)=>{
+//   console.log('s',data);
+// },()=>{
+//   console.log("fail");
+// })
 
 // fs.readFile('./a.txt','UTF8',function (err,data) {
 //   if(err) return console.log(err);
--- source/3.promise.js
+++ source/3.promise.js
@@ -4,12 +4,18 @@
  * @Autor: ziwei
  * @Date: 2021-04-02 10:54:58
  * @LastEditors: ziwei
- * @LastEditTime: 2021-04-02 11:26:10
+ * @LastEditTime: 2021-04-02 13:59:27
  */
 
 let PENDING = 'PENDING';
 let FULLFILLed = 'FULLFIlled';
 let REJECTED = 'REJECTED';
+
+//利用x的值判断时调用promise2的resolve驾驶reject
+function  resolvePromise(promise2,x,resolve,reject){
+  //核心流程
+  console.log(promise2,x,resolve,reject);
+}
 
 class Promise {
   constructor(executor) {
@@ -52,44 +58,57 @@ class Promise {
     let promise2 = new Promise((resolve, reject) => {
       if (this.status == FULLFILLed) {
         //成功调用成功的方法
-        try {
-          let x = onFullfilled(this.value);
-
-          //此x 可能是一个promise 如果是promise需要看一下这个promise是成功还是失败 .then,如果是成功则把成功结果调用promise2的resolve传递进去，如果失败则同理
-
-          //总结 x的值决定是调用promise2的resolve还是reject，如果是promise则取他的状态，如果是普通值则直接调用resolve
-          resolve(x);
-        } catch (e) {
-          reject(e);
-        }
+        //因为：resolvePromise里边用到了promise2 在new的时候还没有生成 所以要开另外的任何 settimeout或者setinterval 等
+        setTimeout(() => {
+          try {
+            let x = onFullfilled(this.value);
+  
+            //此x 可能是一个promise 如果是promise需要看一下这个promise是成功还是失败 .then,如果是成功则把成功结果调用promise2的resolve传递进去，如果失败则同理
+  
+            //总结 x的值决定是调用promise2的resolve还是reject，如果是promise则取他的状态，如果是普通值则直接调用resolve
+            // resolve(x);
+            resolvePromise(promise2,x,resolve,reject)
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
       }
       if (this.status == REJECTED) {
         //失败调用失败的方法
-        try {
-          let x = onRejected(this.reason);
-          resolve(x);
-        } catch (e) {
-          reject(e);
-        }
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            resolvePromise(promise2,x,resolve,reject)
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
       }
       if (this.status == PENDING) {
         //代码是异步调用resolve或者reject
-        try {
           this.onResolvedCallbacks.push(() => {
-            let x = onFullfilled(this.value);
-            resolve(x);
+            setTimeout(() => {
+              try {
+              let x = onFullfilled(this.value);
+              resolvePromise(promise2,x,resolve,reject)
+              } catch (e) {
+                reject(e);
+              }
+            }, 0);
           });
-        } catch (e) {
-          reject(e);
-        }
-        try {
+
+        
           this.onRejectedCallback.push(() => {
-            let x = onRejected(this.reason);
-            resolve(x); //只要返回的普通值都会走resolve
+            setTimeout(() => {
+              try {
+                let x = onRejected(this.reason);
+                resolvePromise(promise2,x,resolve,reject) //只要返回的普通值都会走resolve
+              } catch (e) {
+                reject(e);
+              }
+            })
           });
-        } catch (e) {
-          reject(e);
-        }
+       
       }
     });
     return promise2;
